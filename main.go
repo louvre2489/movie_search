46a490ea9e666d06237c1954c9d51fbe0c4b2861
louvre2489@gmail.com <louvre2489@gmail.com>
2024-02-27 09:23:01 +0900

    実験

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 
 func main() {
   engine := gin.Default()
-  engine.GET("/", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "hello world",
-    })
-  })
+  engine.GET("/", getRoute)
 
-  engine.Run(":3000")
+  engine.Run()
+}
+
+func getRoute(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{
+    "message": "hello world!",
+  })
 }
